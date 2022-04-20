aaf47e46ba25d5a2479cee172190dbefc53024a9
Donald <donngraphics@gmail.com>
2022-04-19 20:51:45 -0500

    Adds Swift 8kyu kataTwist

---
+++ swift/8kyu/kataTwist.swift
@@ -0,0 +1,15 @@
+func kataTwist() {
+var websites = [String]()
+
+var counter = 0
+
+  while counter < 1000 {
+
+    websites.append("codewars")
+    counter += 1
+ 
+  }
+ print(websites.count)
+
+}
+kataTwist()
